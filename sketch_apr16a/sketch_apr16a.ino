9604db976d992553306fd1d9d70fdb18c6cf50fc
Answed <schradearwed@gmail.com>
2023-04-19 14:29:18 +0200

    Update sketch_apr16a.ino
    Added the code for the handbrake

--- sketch_apr16a/sketch_apr16a.ino
+++ sketch_apr16a/sketch_apr16a.ino
@@ -1,3 +1,11 @@
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+  0, 0,                  // Button Count, Hat Switch Count
+  true, false, false,     // X and Y, but no Z Axis
+  false, false, false,   // No Rx, Ry, or Rz
+  false, false,          // No rudder or throttle
+  false, false, false);  // No accelerator, brake, or steering
 
 int potPin = A0;
 int potVal = 0;
@@ -6,11 +14,14 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(potPin, INPUT);
+  Joystick.begin();
+  Joystick.setXAxisRange(0, 1023);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   potVal = analogRead(potPin);
-  Serial.print(potVal);
-  delay(100);
+  Serial.write(potVal);
+  Joystick.setXAxis(potVal);
+  delay(10);
 }
